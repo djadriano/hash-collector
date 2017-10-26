3befe0bb53a893d577e55138bfcb58bbc2e1416a
Adriano Fernandes <djadrianof@gmail.com>
2017-10-26 19:32:00 -0200

    updated README and added bash script

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # hash-collector
 Bash script to collect hash commits via txt file and generate zip of some folder in the repo
+If you need get some folder of repo based on a hash (commit), zip the folder and save this script will make you happy :)
+
+# how to use:
+The script needs the following parameters:
+File.txt / folder to save the zips / folder that will be zipped in project
+
+# example:
+```sh
+$ ./hash-collector.sh file.txt ~/Documents dist/
+```
+
+# format of txt:
+The script read the txt file line by line, so your txt should follow the structure:
+
+```txt
+13200e0b1cec3c0150e53ffc8b3231ebc19e8cc3
+3ef1d4ed247d8d5389f5a419eee9a4f9cf51a644
+3ef1d4ed247d8d5389f5a419eee9a4f9cf51a527
+```
+
+# demo:
+
---
+++ hash-collector.sh
@@ -0,0 +1,167 @@
+# --------------------------------------------------------------------------------------
+# How to Use:
+# The script needs the following parameters:
+# File.txt / folder to save the zips / folder that will be zipped in project
+#
+# Example:
+# $ ./hash.sh file.txt ~/Documents dist/
+# --------------------------------------------------------------------------------------
+
+
+clear
+
+# -----------------------------------------------------
+# Color Variables
+# -----------------------------------------------------
+
+Color_Off='\033[0m'       # Text Reset
+Black='\033[0;30m'        # Black
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Blue='\033[0;34m'         # Blue
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
+White='\033[0;37m'        # White
+
+# -----------------------------------------------------
+# Parameters
+# -----------------------------------------------------
+
+txt_file=$1
+destiny_folder=$2
+folder_to_be_zipped=$3
+
+# -----------------------------------------------------
+# Default variables
+# -----------------------------------------------------
+
+count=1
+next_line=""
+current_branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+# -----------------------------------------------------
+# Methods
+# -----------------------------------------------------
+
+function generate_zip_folder {
+  hash=$1
+
+  echo "\n${Green}Zip folder:${Color_Off}"
+  echo "${White}${bold}${hash}.zip${normal}${Color_Off}"
+
+  zip -qr "$hash.zip" "./$folder_to_be_zipped"
+
+  echo "\n${Green}Copy to:${Color_Off}"
+  echo "${White}${bold}${destiny_folder}${normal}${Color_Off}"
+
+  cp -r "$hash.zip" $destiny_folder
+
+  echo "\n${Blue}Removing zip file:${Color_Off}"
+  echo "${White}${bold}${hash}.zip${normal}${Color_Off}\n"
+
+  rm -r "$hash.zip"
+}
+
+function read_file {
+  while IFS='' read -r hash || [[ -n "$hash" ]]; do
+
+    count=$(($count + 1))
+    next_line=$(sed -n "$count"p $txt_file)
+
+    # check if hash is not null
+    if [ -n "$hash" ]
+    then
+
+      echo "\n${Green}------------------------------------------------------------------------------${Color_Off}"
+      echo "${Green}Reading hash:${Color_Off}"
+      echo "${White}${bold}${hash}${normal}${Color_Off}"
+      echo "${Green}------------------------------------------------------------------------------${Color_Off}\n"
+
+      # check if exist hash in git project
+      if git merge-base --is-ancestor $hash HEAD; then
+        git checkout $hash
+
+        if [ -d "./$folder_to_be_zipped" ]; then
+          generate_zip_folder $hash
+          checkout_branch
+        fi
+      else
+
+        echo "\n${Red}------------------------------------------------------------------------------${Color_Off}"
+        echo "${Red}Hash not found:${Color_Off}"
+        echo "${White}${bold}${hash}${normal}${Color_Off}"
+        echo "${Red}------------------------------------------------------------------------------${Color_Off}\n"
+
+      fi
+    fi
+
+  done < "$txt_file"
+}
+
+function checkout_branch {
+  if [ -n "$next_line" ]
+  then
+      git checkout $next_line
+  else
+      git checkout $current_branch
+
+      echo "\n${Purple}------------------------------------------------------------------------------${Color_Off}"
+      echo "${Purple}Opening destiny folder:${Color_Off}"
+      echo "${White}${bold}${destiny_folder}${normal}${Color_Off}"
+      echo "${Purple}------------------------------------------------------------------------------${Color_Off}\n"
+
+      open $destiny_folder
+  fi
+}
+
+function init {
+
+  if ! [ -d "./.git" ]; then
+    echo "\n${Red}------------------------------------------------------------------------------${Color_Off}"
+    echo "${Red}There is not a git in this project! ${Color_Off}"
+    echo "${Red}------------------------------------------------------------------------------${Color_Off}\n"
+    exit
+  fi
+
+  # check if passed txt file
+  if [ -z "$txt_file" ]
+  then
+    echo "\n${Red}------------------------------------------------------------------------------${Color_Off}"
+    echo "${Red}Please set the .txt file with hashes${Color_Off}"
+    echo "${White}Example: $ ./hash.sh ${bold}name-of-file.txt ${normal}path-to-destiny-folder folder-to-be-zipped${Color_Off}"
+    echo "${Red}------------------------------------------------------------------------------${Color_Off}\n"
+    exit
+  fi
+
+  # check if passed destiny folder
+  if [ -z "$destiny_folder" ]
+  then
+    echo "\n${Red}-----------------------------------------------------------------------${Color_Off}"
+    echo "${Red}Please set the destiny folder${Color_Off}"
+    echo "${White}Example: $ ./hash.sh name-of-file.txt ${bold}path-to-destiny-folder ${normal}folder-to-be-zipped${Color_Off}"
+    echo "${Red}-----------------------------------------------------------------------${Color_Off}\n"
+    exit
+  fi
+
+  # check if passed folder to be zipped
+  if [ -z "$folder_to_be_zipped" ]
+  then
+    echo "\n${Red}-----------------------------------------------------------------------${Color_Off}"
+    echo "${Red}Please set the destiny folder${Color_Off}"
+    echo "${White}Example: $ ./hash.sh name-of-file.txt path-to-destiny-folder ${bold}folder-to-be-zipped${normal}${Color_Off}"
+    echo "${Red}-----------------------------------------------------------------------${Color_Off}\n"
+    exit
+  fi
+
+  read_file
+
+}
+
+# -----------------------------------------------------
+# Start the code
+# -----------------------------------------------------
+
+init
